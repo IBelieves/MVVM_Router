a66fecb164f6654022433b0cb246e0ffdf819258
match <1668126018@qq.com>
2021-01-19 10:58:57 +0800

    Delete BabyRecord directory

--- BabyRecord/core_base/src/main/java/cn/hualand/entity/RecordEntity.java
+++
@@ -1,4 +0,0 @@
-package cn.hualand.entity;
-
-public class RecordEntity {
-}
--- BabyRecord/fun-home/src/main/java/cn/hualand/fun_home/viewmodel/NewRecordModel.kt
+++
@@ -1,9 +0,0 @@
-package cn.hualand.fun_home.viewmodel
-
-/**
- *12/25/20
- *author:hua-land
- *email:huazhengland@gmail.com
- **/
-class NewRecordModel {
-}
